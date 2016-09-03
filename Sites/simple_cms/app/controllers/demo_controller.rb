d735dd92c3e1b4e02430fde8f2696471af6be025
Brett Jenken <brettjenken@hotmail.com>
2016-09-02 22:25:13 -0400

    Played around with links/url params

--- Sites/simple_cms/app/controllers/demo_controller.rb
+++ Sites/simple_cms/app/controllers/demo_controller.rb
@@ -6,5 +6,19 @@ class DemoController < ApplicationController
   end
   
   def hello
+	# render('index');
+	# @ = instance variable, template will have access
+	@array = ["Gucci", "Shmurda", "Weezy", "Max B"]
+	@id = params['id'].to_i
+	@page = params[:page].to_i
+  end
+  
+  def other_hello
+  #demo is unecessary here but shows how to define the controller to redirect to
+	redirect_to(:controller => 'demo', :action => 'hello')
+  end
+  
+  def reddit_link
+	redirect_to("http://reddit.com/r/hiphopheads");
   end
 end
